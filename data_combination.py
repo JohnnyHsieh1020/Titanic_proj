85fa7bd6e0ecc785ce5fed07b4d2e637e499fca6
JohnnyHsieh1020 <46452717+JohnnyHsieh1020@users.noreply.github.com>
2021-06-08 15:12:02 +0800

    update file

--- data_combination.py
+++ data_combination.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 """
 Created on Tue Apr 13 14:18:47 2021
 
 @author: Johnny Hsieh
 """
 import pandas as pd
-import numpy as np 
+import numpy as np
 
 df_train = pd.read_csv('dataset/train.csv')
 df_test = pd.read_csv('dataset/test.csv')
@@ -15,9 +14,10 @@ df_test['train_test'] = 0
 
 df_test['Survived'] = np.NaN
 
-df = pd.concat([df_train,df_test])
+df = pd.concat([df_train, df_test])
 
-columns = df.columns
-size_of_data = {'records':df.shape[0], 'columns':df.shape[1]}
+df.columns
+size_of_data = {'records': df.shape[0], 'columns': df.shape[1]}
+print(size_of_data) # {'records': 1309, 'columns': 13}
 
 df.to_csv('full_data.csv', index=False)
